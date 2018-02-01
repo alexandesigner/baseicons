98ff650d43cf5189030b055ef8ad53fab388cf87
Allan Alexandre <alexandesigner@gmail.com>
2018-02-01 16:52:12 -0300

    Remodeling package.. create a design branding and improve code

--- README.md
+++ README.md
@@ -1,13 +1,31 @@
-# alexandesigner - Icon Pack
-> Version 0.1.0
+<p align="center"><img src="https://github.com/alexandesigner/baseicons/tree/master/src/images/brand.jpg" /></p>
 
-### Instalar
+> This repository is an icon generator in SVG Symbol .. The icons are designed under the [material design guidelines](https://material.io/guidelines/).
+
+### Install
+
 ``$ git clone https://github.com/alexandesigner/baseicons``
 
 ``$ cd baseicons && npm i``
 
-### Rodar localmente
+### Generate
+
+If you want to generate a sprite of customizable icons, simply add the icons in ``.svg`` to the ``/icons`` folder then go to your terminal and type:
+
+``$ gulp svgstore``
+
+> This will generate an SVG file ``icons.svg`` in the ``/src/images`` folder.
+
+### Running to local on PORT:8000
+
 ``$ gulp``
 
-### Gerar os símbolos e concatenar na ``/dist``
-``$ gulp svgstore``
+## Bug?
+
+Submit it in the [issues](https://github.com/alexandesigner/baseicons/issues)
+
+## Get Involved
+
+We'd love for you to help us build Baseicons. If you'd like to be a contributor, check out our <a href="https://github.com/alexandesigner/baseicons/blob/master/.github/CONTRIBUTING.md" target="_blank">Contributing guide</a>
+
+<p>Designed with ♥ by <a target="_blank" href="http://alexandesigner.com.br" title="Allan Alexandre">Allan Alexandre</a>. Licensed under the <a target="_blank" href="https://github.com/alexandesigner/baseicons#license" title="MIT License">MIT License</a>.</p>
--- gulpfile.js
+++ gulpfile.js
@@ -1,70 +1,77 @@
-(function (require) {
-
+(function(require) {
   // Variables
-  var gulp = require('gulp');
-  var watch = require('gulp-watch');
-  var sass = require('gulp-sass');
-  var gutil = require('gulp-util');
-  var connect = require('gulp-connect');
-  var historyApiFallback = require('connect-history-api-fallback');
-  var concat = require('gulp-concat');
-  var changed = require('gulp-changed');
-  var svgstore = require('gulp-svgstore');
-  var svgmin = require('gulp-svgmin');
-  var path = require('path');
+  const gulp = require("gulp"),
+    watch = require("gulp-watch"),
+    sass = require("gulp-sass"),
+    gutil = require("gulp-util"),
+    connect = require("gulp-connect"),
+    historyApiFallback = require("connect-history-api-fallback"),
+    concat = require("gulp-concat"),
+    changed = require("gulp-changed"),
+    svgstore = require("gulp-svgstore"),
+    svgmin = require("gulp-svgmin"),
+    path = require("path");
 
   // Paths
-  var paths = {
-    scripts:  ['./app/js/scripts.js'],
-    scss:     ['./app/src/scss/**/*.scss'],
-    icons:    ['./icons/*.svg']
+  const paths = {
+    scripts: ["./src/js/scripts.js"],
+    scss: ["./src/scss/**/*.scss"],
+    icons: ["./icons/*.svg"]
   };
 
   // Connect Port 8000
-  gulp.task('connect', function() {
+  gulp.task("connect", function() {
     connect.server({
-      root: 'app',
+      root: "src",
       livereload: true,
       port: 8000,
       middleware: function(connect, opt) {
-        return [ historyApiFallback({}) ];
+        return [historyApiFallback({})];
       }
     });
   });
 
   // SCSS Styles
-  gulp.task('styles', function () {
-    return gulp.src(paths.scss)
-      .pipe(sass({outputStyle: 'expanded', errLogToConsole: true}))
-      .pipe(concat('styles.css'))
-      .pipe(gulp.dest('app/css'))
+  gulp.task("styles", function() {
+    return gulp
+      .src(paths.scss)
+      .pipe(sass({ outputStyle: "expanded", errLogToConsole: true }))
+      .pipe(concat("styles.css"))
+      .pipe(gulp.dest("src/css"))
       .pipe(connect.reload());
   });
 
   // Watch
-  gulp.task('watch', function() {
-    gulp.watch(paths.scss, ['styles']);
+  gulp.task("watch", function() {
+    gulp.watch(paths.scss, ["styles"]);
   });
 
   // SVG Generate and Minify
-  gulp.task('svgstore', function () {
-    return gulp.src(paths.icons)
-        .pipe(svgmin(function (file) {
-            var prefix = path.basename(file.relative, path.extname(file.relative));
-            return {
-                plugins: [{
-                    cleanupIDs: {
-                        prefix: prefix + '-',
-                        minify: true
-                    }
-                }]
-            }
-        }))
-        .pipe(svgstore())
-        .pipe(gulp.dest('./dest'));
+  gulp.task("svgstore", function() {
+    return gulp
+      .src(paths.icons)
+      .pipe(
+        svgmin(function(file) {
+          var prefix = path.basename(
+            file.relative,
+            path.extname(file.relative)
+          );
+          return {
+            plugins: [
+              {
+                cleanupIDs: {
+                  prefix: prefix + "-",
+                  minify: true
+                }
+              }
+            ]
+          };
+        })
+      )
+      .pipe(svgstore())
+      .pipe(gulp.dest("./src/images"));
   });
 
   // Running Watch
-  gulp.task('default', [ 'styles', 'watch', 'connect' ]);
-
-}(require));
+  gulp.task("default", ["styles", "watch", "connect"]);
+})(require);
